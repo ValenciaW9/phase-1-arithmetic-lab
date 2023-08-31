0f5785e67f5357c94c183e69c22af295b1b352f0
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-08-31 19:11:44 -0400

    Phase 1 Arithmetic lab
    Phase 1 Arithmetic lab

--- phase-1-arithmetic-lab-master/index.js
+++ phase-1-arithmetic-lab-master/index.js
@@ -1,33 +1,14 @@
-require ( './helpers.js' );
-describe('index.js', function() {
-       describe('multiply', function() {
-         it("is an equation whose multiplied results will equal 62", function() {
-           expect('num1').to.be.a('number')
-           expect('num2').to.be.a('number')
-           expect(multiply).to.eq(62)
-         });
-       });
-       expect('num3').to.be.a('number')
-       expect('num4').to.be.a('number')
-       expect('mod').to.eq(4)
-       describe('mod', function() {
-              it("is an equation that calculates a remainder that is equal to 4", function() {
-                expect('num3').to.be.a('number')
-                expect('num4').to.be.a('number')
-                expect('mod').to.eq(4)
-              });
-            });
-       describe('random', function() {
-              it("generates a random integer greater than 0", function() {
-                expect(Number.isInteger('random')).to.be.true
-                expect('random').to.not.eq(0)
-              });
-            });
-            describe('max', function() {
-              it("will return 20 as the highest number in the set", function() {
-                expect('max').to.eq(20)
-              });
-            });
-          });
-          
+let num1 = 31;
+let num2 = 2;
+let multiply = nim1 * num2;
+console.log(multiply); //Output: 62
+let random =Math.floor(Math.random() * 100) + 1;
+console.log(random); //  Output: random number between 1 and 100
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod); //Output: 4
+let max = Math.max(10, 20, 5);
+console.log(max); //Output 20
+
             
